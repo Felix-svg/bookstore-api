85d28c41ceafc0f3299795e2e687f911eeb8c751
Felix Omondi <felixomosh7@gmail.com>
2024-12-06 11:58:17 +0300

    Define CustomerController methods

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,3 +1,4 @@
+using BookStoreAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +9,45 @@ namespace BookStoreAPI.Controllers
     public class CustomerController : ControllerBase
     {
         // GET /api/customers
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+
+        }
+
         // GET /api/customers/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetCustomer(int id)
+        {
+
+        }
+
         // POST /api/customers
+        [HttpPost]
+        public IActionResult PostCustomer([FromBody] Customer customer)
+        {
+            
+        }
+
         // PUT /api/customers/{id}
+        [HttpPut("{id}")]
+        public IActionResult PutCustomer()
+        {
+
+        }
+
         // DELETE /api/customers/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(int id)
+        {
+
+        }
+
         // POST /api/customers/{customerId}/{bookId}
+        [HttpPost("{customerId}/{bookId}")]
+        public IActionResult PurchaseBook(int customerId, int bookId)
+        {
+            
+        }
     }
 }
