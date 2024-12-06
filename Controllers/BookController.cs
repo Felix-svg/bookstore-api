512edbaab294e7f7c2b881d2dda9f33306204e4a
Felix Omondi <felixomosh7@gmail.com>
2024-12-06 11:51:07 +0300

    Define BookController methods

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,3 +1,4 @@
+using BookStoreAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,38 @@ namespace BookStoreAPI.Controllers
     public class BookController : ControllerBase
     {
         // GET /api/books
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+
+        }
+
         // GET /api/books/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetBook(int id)
+        {
+
+        }
+
         // POST /api/books
+        [HttpPost]
+        public IActionResult PostBook([FromBody] Book book)
+        {
+
+        }
+
         // PUT /api/books/{id}
+        [HttpPut("{id}")]
+        public IActionResult PutBook([FromBody] Book book, int id)
+        {
+
+        }
+
         // DELETE /api/books/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+
+        }
     }
 }
