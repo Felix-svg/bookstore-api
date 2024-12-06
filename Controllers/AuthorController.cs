23e892d8a5703307199513bce131a242eb5a23a9
Felix Omondi <felixomosh7@gmail.com>
2024-12-06 12:49:02 +0300

    Complete the AuthorController routes

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -12,35 +12,62 @@ namespace BookStoreAPI.Controllers
         [HttpGet]
         public IActionResult GetAuthors()
         {
-
+            var authors = AppDBContext._authorsList;
+            if (!authors.Any())
+            {
+                return NoContent();
+            }
+            return Ok(authors);
         }
 
         // GET /api/authors/{id}
         [HttpGet("{id}")]
         public IActionResult GetAuthor(int id)
         {
-
+            var author = AppDBContext._authorsList.Find(author => author.Id == id);
+            if (author == null)
+            {
+                return NotFound("Author not found");
+            }
+            return Ok(author);
         }
 
         // POST /api/authors
         [HttpPost]
-        public IActionResult PostAuthor([FromBody] Author author)
+        public IActionResult PostAuthor([FromBody] Author newAuthor)
         {
-
+            newAuthor.Id = AppDBContext._authorsList.Count + 1;
+            AppDBContext._authorsList.Add(newAuthor);
+            return CreatedAtAction(nameof(GetAuthor), newAuthor.Id, newAuthor);
         }
 
         // PUT /api/authors/{id}
         [HttpPut]
-        public IActionResult PutAuthor([FromBody] Author author, int id)
+        public IActionResult PutAuthor([FromBody] Author updatedAuthor, int id)
         {
+            var authorOld = AppDBContext._authorsList.Find(author => author.Id == id);
+            if(authorOld == null)
+            {
+                return NotFound("Author not found");
+            }
 
+            authorOld.Name = updatedAuthor.Name;
+            authorOld.Bio = updatedAuthor.Bio;
+            authorOld.Books = updatedAuthor.Books;
+            return Ok(authorOld);
         }
 
         // DELETE /api/authors/{id}
         [HttpDelete("{id}")]
         public IActionResult DeleteAuthor(int id)
         {
-
+            var author = AppDBContext._authorsList.Find(author => author.Id == id);
+            if (author == null)
+            {
+                return NotFound("Author not found");
+            }
+            AppDBContext._authorsList.Remove(author);
+            return Ok(author);
         }
     }
 }
