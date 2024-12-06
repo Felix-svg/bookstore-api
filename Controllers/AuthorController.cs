abe877fcf36ceb8649213c619728f84eb7a8edae
Felix Omondi <felixomosh7@gmail.com>
2024-12-06 11:54:13 +0300

    Define AuthorController methods

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -1,3 +1,4 @@
+using BookStoreAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,38 @@ namespace BookStoreAPI.Controllers
     public class AuthorController : ControllerBase
     {
         // GET /api/authors
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+
+        }
+
         // GET /api/authors/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(int id)
+        {
+
+        }
+
         // POST /api/authors
+        [HttpPost]
+        public IActionResult PostAuthor([FromBody] Author author)
+        {
+
+        }
+
         // PUT /api/authors/{id}
+        [HttpPut]
+        public IActionResult PutAuthor([FromBody] Author author, int id)
+        {
+
+        }
+
         // DELETE /api/authors/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(int id)
+        {
+
+        }
     }
 }
